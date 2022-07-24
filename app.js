04b1bb57491209b301bd7513081967f4e89a43d0
Dan <danjosef90@gmail.com>
2022-07-24 13:41:37 -0700

    Added variables for game

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
-let playerText = document.getElementById('playerText')
-let restartButton = document.getElementById('button')
-let cell = Array.from(document.getElementsByClassName('cell'))
+let playerX = "X";
+let playerO = "O" ;
+let notPlayerX = false  // first player will be X
+let cell = Array.from(document.getElementsByClassName('cell'));
+let board = document.getElementById('board');
+let gameMessage = document.getElementById('gameMessage');
+let restartButton = document.getElementById('restartButton');
+
+let winningCombos = [
+    [0,1,2]
+    [3,4,5]
+    [6,7,8]
+    [0,3,6]
+    [1,4,7]
+    [2,5,8]
+    [0,4,8]
+    [2,4,6]
+]
+
+// console.log(cell);
+
+
+
